a926acf9ce0da191f7d62c98ab5f9ecfd5890525
ashraf9932 <ashraf.neizk@gmail.com>
2024-08-21 02:50:03 +0300

    remove comment form model

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
             <Navbar />
             <Hero />
             <Highlights />
-            {/* <Model /> */}
+            <Model />
             <Features />
             <HowItWorks />
             <Footer />
